66b46c1ffec4cd6d191d62258bdb6e78d10c9c51
RobelBira <robelbira@gmail.com>
2023-11-01 10:17:25 +0300

    d

--- tail/src/components/Portfolio.jsx
+++ tail/src/components/Portfolio.jsx
@@ -4,130 +4,165 @@ import PortImage3 from "../assets/workout.png"
 import PortImage4 from "../assets/shop.png"
 import PortImage5 from "../assets/housemarket.png"
 import PortImage6 from "../assets/portfolio.png"
+import PortImage7 from "../assets/Screenshot 2023-11-01 101323.png";
 const Portfolio = () => {
 
  return (
-    <div name="portfolio"
-    className=" bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full" >
-      <div className=" pt-12">
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">Portfolio</p>
+   <div
+     name="portfolio"
+     className=" bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+   >
+     <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+       <div className=" pt-12">
+         <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
+           Portfolio
+         </p>
 
-        <p className="py-6">check out some of my works right here</p>
-        </div>
-        
+         <p className="py-6">check out some of my works right here</p>
+       </div>
 
+       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
+         {/*  */}
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage7}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full"
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelshopper.vercel.app"> 
+             
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
 
-  <div 
-  className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 ">
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage5} alt=""
-className="rounded-md duration-200 hover:scale-105 w-full"
-/> 
-<div className="flex items-center justify-center">
-  <a href="https://robelhousemarketplace.vercel.app" >
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
- </a>
- <a href="https://github.com/RobelBira">
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
- </a>
-</div>
-</div>
+         {/*  */}
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage5}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full"
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelhousemarketplace.vercel.app">
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
 
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage1} alt=""
-className="rounded-md duration-200 hover:scale-105 w-full "
-/> 
-<div className="flex items-center justify-center">
-   <a href="https://robelgithubfinder.vercel.app" >
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
- </a>
- <a href="https://github.com/RobelBira">
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
- </a>
-</div>
-</div>
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage2} alt=""
-className="rounded-md duration-200 hover:scale-105 "
-/> 
-<div className="flex items-center justify-center">
-   <a href="https://robelfeedbackapp.netlify.app" >
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage1}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full "
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelgithubfinder.vercel.app">
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage2}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 "
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelfeedbackapp.netlify.app">
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
 
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
-   </a>
-    <a href="https://github.com/RobelBira">
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage6}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full"
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelportfolio.netlify.app">
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
 
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
-    </a>
-</div>
-</div>
-
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage6} alt=""
-className="rounded-md duration-200 hover:scale-105 w-full"
-/> 
-<div className="flex items-center justify-center">
-   <a href="https://robelportfolio.netlify.app" >
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
- </a>
-  <a href="https://github.com/RobelBira">
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
-  </a>
-    
-</div>
-</div>
-
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage4} alt=""
-className="rounded-md duration-200 hover:scale-105 w-full"
-/> 
-<div className="flex items-center justify-center">
-   <a href="https://robelelectronicsshop.netlify.app" >
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
- </a>
-  <a href="https://github.com/RobelBira">
-
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
-  </a>
-</div>
-</div>
-<div
-className="shadow-md shadow-gray-600 rounded-lg">
-<img 
-src={PortImage3} alt=""
-className="rounded-md duration-200 hover:scale-105 w-full"
-/> 
-<div className="flex items-center justify-center">
- <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
-  <a href="https://github.com/RobelBira">
-
- <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
-  </a>
-</div>
-</div>
-
-
- 
-
-</div>
-
-
-
-
-     </div>  
-     
-    </div>
-  )
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage4}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full"
+           />
+           <div className="flex items-center justify-center">
+             <a href="https://robelelectronicsshop.netlify.app">
+               <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Demo
+               </button>
+             </a>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
+         <div className="shadow-md shadow-gray-600 rounded-lg">
+           <img
+             src={PortImage3}
+             alt=""
+             className="rounded-md duration-200 hover:scale-105 w-full"
+           />
+           <div className="flex items-center justify-center">
+             <button className="w-1/2  px-6 py-3 m-4 duration-200 hover:scale-105">
+               Demo
+             </button>
+             <a href="https://github.com/RobelBira">
+               <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                 Code
+               </button>
+             </a>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 }
 export default Portfolio
